17105041fc15edb9863d7ed1b55067da1ddb9d39
Andrew White <andyw@pixeltrix.co.uk>
2012-02-14 14:12:44 +0000

    Use self.table_name as set_table_name is deprecated

--- test/support/models.rb
+++ test/support/models.rb
@@ -2,7 +2,7 @@ class Mixin < ActiveRecord::Base
 end
 
 class ListMixin < ActiveRecord::Base
-  set_table_name "mixins"
+  self.table_name = "mixins"
   default_scope order(:pos)
   acts_as_list :column => "pos", :scope => :parent
 end
@@ -14,26 +14,26 @@ class ListMixinSub2 < ListMixin
 end
 
 class ListWithStringScopeMixin < ActiveRecord::Base
-  set_table_name "mixins"
+  self.table_name =  "mixins"
   default_scope order(:pos)
   acts_as_list :column => "pos", :scope => 'parent_id = #{parent_id}'
 end
 
 class ArrayScopeListMixin < ActiveRecord::Base
-  set_table_name "mixins"
+  self.table_name =  "mixins"
   default_scope order(:pos)
   acts_as_list :column => "pos", :scope => [:parent_id, :parent_type]
 end
 
 class AssociationScopeListMixin < ActiveRecord::Base
-  set_table_name "mixins"
+  self.table_name =  "mixins"
   belongs_to :parent
   default_scope order(:pos)
   acts_as_list :column => "pos", :scope => :parent
 end
 
 class PolymorphicAssociationScopeListMixin < ActiveRecord::Base
-  set_table_name "mixins"
+  self.table_name =  "mixins"
   belongs_to :parent, :polymorphic => true
   default_scope order(:pos)
   acts_as_list :column => "pos", :scope => :parent
